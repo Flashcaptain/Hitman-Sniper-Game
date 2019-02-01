5f82fa7af06eed8d822e6691453d5b14068bf080
m.eind <36227686+4561351658456@users.noreply.github.com>
2019-02-01 11:48:39 +0100

    Add simple target description menu and downloaded assets

---
+++ Assets/TargetDescription.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDescription : MonoBehaviour
+{
+    public Transform canvas;
+
+    void Update()
+    {
+        IsToggled();
+    }
+
+    public void Pause()
+    {
+        if (!canvas.gameObject.activeInHierarchy)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+
+        else if (canvas.gameObject.activeInHierarchy)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void IsToggled()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Pause();
+        }
+    }
+}
