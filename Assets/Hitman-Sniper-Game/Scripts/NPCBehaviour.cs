f321eb2bd3f35e2b0f6149a1700e01869c6d2226
Mima <www.mimaeindhoven2000@gmail.com>
2019-02-04 17:21:58 +0100

    Add Targetdesctiption and start NPC behaviour, also added randomized targets

---
+++ Assets/Hitman-Sniper-Game/Scripts/NPCBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehaviour : MonoBehaviour
+{
+    public bool isTarget = false;
+
+    public List<string> targetDescriptions;
+
+    public void OnHit()
+    {
+        if (isTarget)
+        {
+            Debug.Log("VICTORY!!!!!!!!!!!!!");
+        }
+        else
+        {
+            Debug.Log("U HIT AN INNOCENT!!!!!!!!!!!!!");
+        }
+    }
+}
---
+++ Assets/Hitman-Sniper-Game/Scripts/TargetDescription.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetDescription : MonoBehaviour
+{
+    [SerializeField]
+    private Transform canvas;
+
+    [SerializeField]
+    private Transform noteBookText;
+
+    [SerializeField]
+    private List<Text> descriptionText;
+
+    [SerializeField]
+    private List<NPCBehaviour> Targets;
+
+    void Update()
+    {
+        IsToggled();
+    }
+
+    public void Start()
+    {
+        PickTarget();
+    }
+
+    public void PickTarget()
+    {
+        if (Targets.Count == 0)
+        {
+            return;
+        }
+
+        NPCBehaviour target = Targets[Random.Range(0, Targets.Count)];
+        for (int i = 0; i < target.targetDescriptions.Count; i++)
+        {
+            descriptionText[i].text = target.targetDescriptions[i];
+        }
+
+        target.isTarget = true;
+    }
+
+    public void OpenNotebook()
+    {
+        if (!canvas.gameObject.activeInHierarchy)
+        {
+            canvas.gameObject.SetActive(true);
+            noteBookText.gameObject.SetActive(false);
+        }
+
+        else if (canvas.gameObject.activeInHierarchy)
+        {
+            canvas.gameObject.SetActive(false);
+            noteBookText.gameObject.SetActive(true);
+        }
+    }
+
+    public void IsToggled()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OpenNotebook();
+        }
+    }
+}
